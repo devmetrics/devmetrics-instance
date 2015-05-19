8e46e62016d8e581f35e19774a778d2d565a9cfb
Vladimir Nazarov <vladimir@devmetrics.io>
2015-05-19 04:10:17 +0300

    save generate user results in log file

--- user_generate.sh
+++ user_generate.sh
@@ -7,4 +7,5 @@ password=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
 
 sudo htpasswd -b /etc/nginx/htpasswd.users $user $password
 
+echo $(date) $user":"$password >> .log.txt
 echo $user":"$password
