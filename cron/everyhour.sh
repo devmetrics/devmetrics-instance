48d7b5f19187784410814f6e1293ed3533f07e05
Vladimir Nazarov <vladimir@devmetrics.io>
2015-06-02 00:28:01 +0300

    retention by task once per day

--- cron/everyhour.sh
+++ cron/everyhour.sh
@@ -5,4 +5,4 @@
 
 
 # retention policy for our "app-*" indices 7 day
-sudo curator --logfile /opt/log/curator/curator.log --loglevel INFO delete indices --older-than 7 --time-unit days --timestring '%Y.%m.%d' --prefix app-
+# sudo curator --logfile /opt/log/curator/curator.log --loglevel INFO delete indices --older-than 7 --time-unit days --timestring '%Y.%m.%d' --prefix app-
